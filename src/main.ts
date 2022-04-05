71ff0da08cdb46474d6d7c499dad14c2eca1dba4
waiyan <htetwaiyan@better.hr>
2022-04-05 12:15:09 +0630

    expection filter ok

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -17,6 +18,8 @@ async function bootstrap() {
 			}
 		})
 	);
+	// import global filter
+	app.useGlobalFilters(new HttpExceptionFilter)
 	await app.listen(3000);
 }
 bootstrap();
